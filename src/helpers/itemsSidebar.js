cc23c6f852ff0cc222cc3058714e65c5e1aca2b2
Sean Hong <iamseanhong@gmail.com>
2018-04-27 15:08:39 -0700

    Clean up some code at Button.js

--- src/helpers/itemsSidebar.js
+++ src/helpers/itemsSidebar.js
@@ -2,5 +2,4 @@ export default [
   { text: 'Svg Icons', linkName: '/' }, 
   { text: 'Buttons', linkName: 'buttons' },
   { text: 'JSON Styles 1', linkName: 'json-styles-1' },
-  { text: 'JSON Styles 2', linkName: 'json-styles-2' },
 ];
--- src/pages/buttons.js
+++ src/pages/buttons.js
@@ -52,58 +52,15 @@ const StyledSyntax = styled(SyntaxHighlighter)`
   
 `;
 
-const codeString = `<Wrapper>
-    <Button> Default </Button>
-    <Button primary> Primary </Button>
-    <Button secondary> Secondary </Button>
-    <Button tertiary> Tertiary </Button>
-    <Button remove> Remove </Button>
-    <Button isDisabled> Disable </Button>
-    <Button isLoading> Loading </Button>
-    <StyledSyntax language="javascript" style={docco}>
-      {codeString}
-    </StyledSyntax>;
-  </Wrapper>
-`;
+const styleNames = [dark];
 
-
-
-const styleNames = [
-  coy,
-dark,
-funky,
-okaidia,
-solarizedlight,
-tomorrow,
-twilight,
-prism,
-atomDark,
-base16AteliersulphurpoolLight,
-cb,
-darcula,
-duotoneDark,
-duotoneEarth,
-duotoneForest,
-duotoneLight,
-duotoneSea,
-duotoneSpace,
-ghcolors,
-hopscotch,
-pojoaque,
-vs,
-xonokai,
-];
-
-const PRE = 'pre[class*="language-"]'
-const CODE = 'code[class*="language-"]'
+const PRE = 'pre[class*="language-"]';
+const CODE = 'code[class*="language-"]';
 
 dark[PRE]['background'] = 'var(--bg-color-sidebar)';
 dark[PRE]['border'] = 'none';
 dark[PRE]['borderRadius'] = 'none';
 dark[CODE]['textShadow'] = 'none';
-
-console.log('dark: ', dark[PRE]);
-
 
 const Buttons = () => {
   return (
@@ -115,13 +72,13 @@ const Buttons = () => {
       <Button remove> Remove </Button>
       <Button isDisabled> Disable </Button>
       <Button isLoading> Loading </Button>
-      {styleNames.map((styleName, idx) => {
+      {/* {styleNames.map((styleName, idx) => {
         return (
           <StyledSyntax key={idx} language="jsx" style={styleName}>
             {codeString}
           </StyledSyntax>
         );
-      })}
+      })} */}
     </Wrapper>
   );
 };
