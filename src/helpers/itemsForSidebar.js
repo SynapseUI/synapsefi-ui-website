0343825063bc776b02176f42bdc4bfe8884fb09b
Sean Hong <iamseanhong@gmail.com>
2018-04-30 16:44:27 -0700

    Fix route issue

--- src/helpers/itemsForSidebar.js
+++ src/helpers/itemsForSidebar.js
@@ -1,17 +1,26 @@
-export const STYLE_GUIDE = 'styleGuide';
+export const STYLE_GUIDE = 'style-guide';
 export const COMPONENTS = 'components';
 
-export const STYLE_GUIDE_ICONS = 'icons'
-export const STYLE_GUIDE_COLOR = 'colors'
-export const STYLE_GUIDE_BUTTONS = 'buttons'
+export const STYLE_GUIDE_ICONS = 'icons';
+export const STYLE_GUIDE_COLOR = 'colors';
+export const STYLE_GUIDE_BUTTONS = 'buttons';
 
 export default {
   [STYLE_GUIDE]: {
     sectionText: 'Style Guide',
     subSections: {
-      [STYLE_GUIDE_ICONS]: { subSectionText: 'Svg Icons', linkName: `${STYLE_GUIDE}-${STYLE_GUIDE_ICONS}` },
-      [STYLE_GUIDE_COLOR]: { subSectionText: 'Colors', linkName: `${STYLE_GUIDE}-${STYLE_GUIDE_COLOR}` },
-      [STYLE_GUIDE_BUTTONS]: { subSectionText: 'Buttons', linkName: `${STYLE_GUIDE}-${STYLE_GUIDE_BUTTONS}` },
+      [STYLE_GUIDE_ICONS]: {
+        subSectionText: 'Svg Icons',
+        linkName: `/${STYLE_GUIDE}/${STYLE_GUIDE_ICONS}/`,
+      },
+      [STYLE_GUIDE_COLOR]: {
+        subSectionText: 'Colors',
+        linkName: `/${STYLE_GUIDE}/${STYLE_GUIDE_COLOR}/`,
+      },
+      [STYLE_GUIDE_BUTTONS]: {
+        subSectionText: 'Buttons',
+        linkName: `/${STYLE_GUIDE}/${STYLE_GUIDE_BUTTONS}/`,
+      },
     },
   },
   [COMPONENTS]: {
