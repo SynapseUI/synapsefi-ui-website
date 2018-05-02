ad72aaa37db417fb2ab97ce4889e72d83a2381a7
Sean Hong <iamseanhong@gmail.com>
2018-05-02 14:37:21 -0700

    Move npm-components to components dir

--- src/pages/style-guide/buttons.js
+++ src/pages/style-guide/buttons.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import Button from '../../npm-components/Button';
+import { Button } from '../../npm-components/export';
 // -----------------------------------------------------------------------------------------
 // ------------------------------------ Styled Components ----------------------------------
 // -----------------------------------------------------------------------------------------
