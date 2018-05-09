6334e44a1578d28377f6f5c329dfb468888aed0b
Sean Hong <iamseanhong@gmail.com>
2018-05-09 16:48:28 -0700

    Change sidebar to alpha order except getting started

--- src/helpers/dataForSidebar.js
+++ src/helpers/dataForSidebar.js
@@ -19,17 +19,17 @@ export default {
         [SUB_SECTION_TEXT]: 'Getting Started',
         [SUB_SECTION_LINK]: `/${BASICS}/${BASICS_GETTING_STARTED}/`,
       },
-      [BASICS_ICONS]: {
-        [SUB_SECTION_TEXT]: 'Svg Icons',
-        [SUB_SECTION_LINK]: `/${BASICS}/${BASICS_ICONS}/`,
+      [BASICS_BUTTONS]: {
+        [SUB_SECTION_TEXT]: 'Buttons',
+        [SUB_SECTION_LINK]: `/${BASICS}/${BASICS_BUTTONS}/`,
       },
       [BASICS_COLOR]: {
         [SUB_SECTION_TEXT]: 'Colors',
         [SUB_SECTION_LINK]: `/${BASICS}/${BASICS_COLOR}/`,
       },
-      [BASICS_BUTTONS]: {
-        [SUB_SECTION_TEXT]: 'Buttons',
-        [SUB_SECTION_LINK]: `/${BASICS}/${BASICS_BUTTONS}/`,
+      [BASICS_ICONS]: {
+        [SUB_SECTION_TEXT]: 'Svg Icons',
+        [SUB_SECTION_LINK]: `/${BASICS}/${BASICS_ICONS}/`,
       },
     },
   },
