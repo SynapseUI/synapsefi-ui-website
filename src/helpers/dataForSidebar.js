6d2318a51d5630ea9aedd3ed12d7e2650c8df571
Sean Hong <iamseanhong@gmail.com>
2018-05-14 16:51:21 -0700

    Simplify dataForSidebar code

--- src/helpers/dataForSidebar.js
+++ src/helpers/dataForSidebar.js
@@ -3,50 +3,53 @@ export const SUB_SECTIONS = 'SUB_SECTIONS';
 export const SUB_SECTION_TEXT = 'SUB_SECTIONS';
 export const SUB_SECTION_LINK = 'SUB_SECTION_LINK';
 
+// -------------------------------------------------------------------------
+
 export const BASICS = 'basics';
 export const COMPONENTS = 'components';
+
+// -------------------------------------------------------------------------
 
 export const BASICS_GETTING_STARTED = 'getting-started';
 export const BASICS_BUTTON_GUIDELINE = 'button-guideline';
 export const BASICS_COLOR = 'color-list';
 export const BASICS_SVG_ICON_LIST = 'svg-icon-list';
 
+const dataForBasicsSection = [
+  { text: 'Getting Started', link: BASICS_GETTING_STARTED },
+  { text: 'Button Guideline', link: BASICS_BUTTON_GUIDELINE },
+  { text: 'Color List', link: BASICS_COLOR },
+  { text: 'Svg Icon List', link: BASICS_SVG_ICON_LIST },
+];
+
+// -------------------------------------------------------------------------
+
 export const COMPONENTS_BUTTON = 'button';
 export const COMPONENTS_SVG_ICONS = 'svg-icons';
+export const COMPONENTS_TABLE = 'table';
 
-export default {
-  [BASICS]: {
-    [SECTION_TEXT]: 'Basics',
-    [SUB_SECTIONS]: {
-      [BASICS_GETTING_STARTED]: {
-        [SUB_SECTION_TEXT]: 'Getting Started',
-        [SUB_SECTION_LINK]: `/${BASICS}/${BASICS_GETTING_STARTED}/`,
-      },
-      [BASICS_BUTTON_GUIDELINE]: {
-        [SUB_SECTION_TEXT]: 'Button Guideline',
-        [SUB_SECTION_LINK]: `/${BASICS}/${BASICS_BUTTON_GUIDELINE}/`,
-      },
-      [BASICS_COLOR]: {
-        [SUB_SECTION_TEXT]: 'Color List',
-        [SUB_SECTION_LINK]: `/${BASICS}/${BASICS_COLOR}/`,
-      },
-      [BASICS_SVG_ICON_LIST]: {
-        [SUB_SECTION_TEXT]: 'Svg Icon List',
-        [SUB_SECTION_LINK]: `/${BASICS}/${BASICS_SVG_ICON_LIST}/`,
-      },
-    },
-  },
-  [COMPONENTS]: {
-    [SECTION_TEXT]: 'Components',
-    [SUB_SECTIONS]: {
-      [COMPONENTS_BUTTON]: {
-        [SUB_SECTION_TEXT]: 'Button',
-        [SUB_SECTION_LINK]: `/${COMPONENTS}/${COMPONENTS_BUTTON}/`,
-      },
-      [COMPONENTS_SVG_ICONS]: {
-        [SUB_SECTION_TEXT]: 'Svg Icons',
-        [SUB_SECTION_LINK]: `/${COMPONENTS}/${COMPONENTS_SVG_ICONS}/`,
-      },
-    },
-  },
+const dataForComponentsSection = [
+  { text: 'Button', link: COMPONENTS_BUTTON },
+  { text: 'Svg Icons', link: COMPONENTS_SVG_ICONS },
+  { text: 'Table', link: COMPONENTS_TABLE },
+];
+
+// -------------------------------------------------------------------------
+
+const injectSubSectionData = (subsectionData, subSectionsObj) => {
+  subsectionData.forEach(({ text, link }) => {
+    subSectionsObj[link] = {};
+    subSectionsObj[link][SUB_SECTION_TEXT] = text;
+    subSectionsObj[link][SUB_SECTION_LINK] = link;
+  });
 };
+
+const dataForSidebar = {
+  [BASICS]: { [SECTION_TEXT]: 'Basics', [SUB_SECTIONS]: {} },
+  [COMPONENTS]: { [SECTION_TEXT]: 'Components', [SUB_SECTIONS]: {} },
+};
+
+injectSubSectionData(dataForBasicsSection, dataForSidebar[BASICS][SUB_SECTIONS]);
+injectSubSectionData(dataForComponentsSection, dataForSidebar[COMPONENTS][SUB_SECTIONS]);
+
+export default dataForSidebar;
