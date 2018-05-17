082bd3e7ea0bb491c363e2f63e3b234943795b4a
Sean Hong <iamseanhong@gmail.com>
2018-05-17 16:42:36 -0700

    Add SandboxTest Component for testing purpose

---
+++ sandbox-components/SandboxTest.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SandboxTest extends Component {
+  render() {
+    return (
+      <div>
+        <div>sandbox test</div>
+      </div>
+    );
+  }
+}
+
+export default SandboxTest;
