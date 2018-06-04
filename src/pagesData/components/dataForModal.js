a297ba97f39eb02c4ac1b53ac3e6b16d57b99220
Sean Hong <iamseanhong@gmail.com>
2018-06-04 16:32:12 -0700

    Update diff color size modal example

--- src/pagesData/components/dataForModal.js
+++ src/pagesData/components/dataForModal.js
@@ -1,13 +1,16 @@
 import JsxModalBasic, * as jsxModalBasic from '../../components/pages/components/modal/JsxModalBasic';
 import JsxModalWithBtns, * as jsxModalWithBtns from '../../components/pages/components/modal/JsxModalWithBtns';
+import JsxModalSizesColors, * as jsxModalSizesColors from '../../components/pages/components/modal/JsxModalSizesColors';
 
 // -----------------------------------------------------------------------------------------
 // ------------------------------------ Data -----------------------------------------------
 // -----------------------------------------------------------------------------------------
 const BASIC = 'Basic';
 const WITH_BTNS = 'With Buttons';
+const DIFF_SIZES_COLORS = 'Diff Sizez & Colors';
 
 export default [
   { title: BASIC, code: jsxModalBasic.jsxSnippet, Component: JsxModalBasic },
   { title: WITH_BTNS, code: jsxModalWithBtns.jsxSnippet, Component: JsxModalWithBtns },
+  { title: DIFF_SIZES_COLORS, code: jsxModalSizesColors.jsxSnippet, Component: JsxModalSizesColors },
 ];
