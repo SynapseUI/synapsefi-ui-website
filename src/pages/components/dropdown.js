a4a1f4d793874e645193dbd9514ffd3f89708c87
Colin Ritchey <colinritchey256@gmail.com>
2018-06-21 12:08:10 -0700

    Added prop values table data to Accordion, DropdownHead/Content. Added Accordion page.

--- src/pages/components/dropdown.js
+++ src/pages/components/dropdown.js
@@ -3,6 +3,9 @@ import React from 'react';
 import SubSectionPageForComponents from '../../components/common/SubSectionPageForComponents';
 
 import propValuesForDropdown from '../../pagesData/propValues/propValuesForDropdownData';
+import propValuesForDropdownHead from '../../pagesData/extra/propValuesForDropdownHead';
+import propValuesForDropdownContent from '../../pagesData/extra/propValuesForDropdownContent';
+
 import dataForDropdown from '../../pagesData/components/dataForDropdown';
 
 const dropdown = () => {
@@ -12,7 +15,15 @@ const dropdown = () => {
         dataForThisPage={dataForDropdown}
         tableData={
           [
-            { data: propValuesForDropdown }
+            { data: propValuesForDropdown },
+            {
+              title: 'Values for DropdownHead',
+              data: propValuesForDropdownHead
+            },
+            {
+              title: 'Values for DropdownContent',
+              data: propValuesForDropdownContent
+            }
           ]
         }
       />
---
+++ src/pagesData/extra/propValuesForDropdownContent.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    name: 'style',
+    type: 'Object',
+    description: 'Inline styles.'
+  },
+  {
+    name: 'showContent',
+    type: 'Boolean',
+    description: 'Displays content if true.'
+  },
+  {
+    name: 'verticalOffset',
+    type: 'String',
+    description: 'Measurement of how far down the content should displayed. I.e. verticalOffset="16px".'
+  },
+  {
+    name: 'children',
+    type: 'HTML Elements',
+    description: 'Nest elements that will render to the left of the down arrow.'
+  },
+]
